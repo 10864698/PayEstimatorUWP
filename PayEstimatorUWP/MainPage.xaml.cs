389b5280a9a6cdb2b7ac05f33ef8d49eee68ed63
10864698 <george.manning@student.uts.edu.au>
2017-01-05 06:25:23 +1100

    Test class added

--- PayEstimatorUWP/MainPage.xaml.cs
+++ PayEstimatorUWP/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -28,72 +29,68 @@ namespace PayEstimatorUWP
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            Initialize();
+            InitializeComponent();
+            Test test = new Test();
+            tbTest.DataContext = test;
+
         }
 
-        private void Initialize()
+        public void Initialize()
         {
             GigsLastWeek();
         }
 
-        private async void GigsLastWeek()
+        public async void GigsLastWeek()
         {
             AppointmentStore appointmentStore = await AppointmentManager.RequestStoreAsync(AppointmentStoreAccessType.AllCalendarsReadOnly);
 
-            DayOfWeek weekStart = DayOfWeek.Monday;
-            DateTime startingDate = DateTime.Today;
+            //DayOfWeek weekStart = DayOfWeek.Monday;
+            //DateTime startingDate = DateTime.Today;
 
-            while (startingDate.DayOfWeek != weekStart)
-                startingDate = startingDate.AddDays(-1);
+            //while (startingDate.DayOfWeek != weekStart)
+            //    startingDate = startingDate.AddDays(-1);
 
-            DateTime previousWeekStart = startingDate.AddDays(-7);
-            DateTime previousWeekEnd = startingDate.AddDays(0);
+            //DateTime previousWeekStart = startingDate.AddDays(-7);
+            //DateTime previousWeekEnd = startingDate.AddDays(0);
 
-            var appCalendars = await appointmentStore.FindAppointmentsAsync(previousWeekStart, TimeSpan.FromDays(7));
+            //var appCalendars = await appointmentStore.FindAppointmentsAsync(previousWeekStart, TimeSpan.FromDays(7));
 
-            if (appCalendars != null)
-            {
-                var gigs = new List<Gig>();
+            //if (appCalendars != null)
+            //{
+            //    List<Gig> gigs = new List<Gig>();
 
-                foreach (var appt in appCalendars)
-                {
-                    try
-                    {
-                        if (appt.Details.Substring(0, 18) == "CrewOnCall::LEVEL3")
-                            gigs.Add(new Gig(appt.StartTime, appt.Duration, appt.Subject, appt.Location, "LEVEL3"));
-                        else if (appt.Details.Substring(0, 18) == "CrewOnCall::VANDVR")
-                            gigs.Add(new Gig(appt.StartTime, appt.Duration, appt.Subject, appt.Location, "VANDVR"));
-                        else if (appt.Details.Substring(0, 17) == "CrewOnCall::MR/HR")
-                            gigs.Add(new Gig(appt.StartTime, appt.Duration, appt.Subject, appt.Location, "MR/HR"));
-                        else
-                            gigs.Add(new Gig(appt.StartTime, appt.Duration, appt.Subject, appt.Location, "LEVEL3"));
-                    }
-                    catch (ArgumentOutOfRangeException)
-                    {
-                        gigs.Add(new Gig(appt.StartTime, appt.Duration, appt.Subject, appt.Location, "LEVEL3"));
-                    }
-                }
+            //    foreach (var appt in appCalendars)
+            //    {
+            //        try
+            //        {
+            //            if (appt.Details.Substring(0, 18) == "CrewOnCall::LEVEL3")
+            //                gigs.Add(new Gig(appt.StartTime, appt.Duration, appt.Subject, appt.Location, "LEVEL3"));
+            //            else if (appt.Details.Substring(0, 18) == "CrewOnCall::VANDVR")
+            //                gigs.Add(new Gig(appt.StartTime, appt.Duration, appt.Subject, appt.Location, "VANDVR"));
+            //            else if (appt.Details.Substring(0, 17) == "CrewOnCall::MR/HR")
+            //                gigs.Add(new Gig(appt.StartTime, appt.Duration, appt.Subject, appt.Location, "MR/HR"));
+            //            else
+            //                gigs.Add(new Gig(appt.StartTime, appt.Duration, appt.Subject, appt.Location, "LEVEL3"));
+            //        }
+            //        catch (ArgumentOutOfRangeException)
+            //        {
+            //            gigs.Add(new Gig(appt.StartTime, appt.Duration, appt.Subject, appt.Location, "LEVEL3"));
+            //        }
+            //    }
 
-                lvGigs.ItemsSource = gigs;
+            //    lvGigs.ItemsSource = gigs;
 
-                var hours = new Hours(gigs);
-                lvHours.DataContext = hours;
+            //    Hours hours = new Hours(gigs);
+            //    tbHours.DataContext = hours;
 
-                var gross = new Gross(gigs);
-                lvGross.DataContext = gross;
-            }
+            //    Gross gross = new Gross(gigs);
+            //    tbGross.DataContext = gross;
         }
-
     }
 
-    class Gig
+    public class Gig
     {
-        private DateTimeOffset startTime;
-        private TimeSpan duration;
-        private string subject;
-        private string location;
-        private string skill;
-
         public string StartDate { get; set; }
         public string StartTime { get; set; }
         public string EndTime { get; set; }
@@ -113,19 +110,13 @@ namespace PayEstimatorUWP
 
         public Gig(DateTimeOffset startTime, TimeSpan duration, string subject, string location, string v)
         {
-            this.startTime = startTime;
-            this.duration = duration;
-            this.subject = subject;
-            this.location = location;
-            this.skill = v;
-
             StartDate = startTime.ToString("D");
             StartTime = startTime.ToString("t");
             EndTime = startTime.Add(duration).ToString("t");
             ClientName = subject;
             VenueName = location;
             Hours = 0;
-            Skill = skill;
+            Skill = v;
             LEVEL3A = 0;
             LEVEL3B = 0;
             LEVEL3SUN = 0;
@@ -140,7 +131,6 @@ namespace PayEstimatorUWP
             var calcHours = new DateTimeOffset();
 
             span = duration;
-            //MessageBox.Show(calcHours.ToString() + "\n" + span.ToString());
             calcHours = startTime;
 
             if ((span < new TimeSpan(3, 0, 0)) && (startTime.DayOfWeek != DayOfWeek.Sunday))
@@ -159,31 +149,31 @@ namespace PayEstimatorUWP
                 if ((calcHours.Hour > 7) && (calcHours.Hour < 20) && (calcHours.DayOfWeek != DayOfWeek.Sunday))
                 {
                     Hours += 0.25;
-                    if ((skill == null) || (skill == "LEVEL3"))
+                    if ((v == null) || (v == "LEVEL3"))
                         LEVEL3A += 0.25;
-                    if (skill == "VANDVR")
+                    if (v == "VANDVR")
                         VANDVRA += 0.25;
-                    if (skill == "MR/HR")
+                    if (v == "MR/HR")
                         MRHRA += 0.25;
                 }
                 if (((calcHours.Hour < 8) || (calcHours.Hour > 19)) && (calcHours.DayOfWeek != DayOfWeek.Sunday))
                 {
                     Hours += 0.25;
-                    if ((skill == null) || (skill == "LEVEL3"))
+                    if ((v == null) || (v == "LEVEL3"))
                         LEVEL3B += 0.25;
-                    if (skill == "VANDVR")
+                    if (v == "VANDVR")
                         VANDVRB += 0.25;
-                    if (skill == "MR/HR")
+                    if (v == "MR/HR")
                         MRHRB += 0.25;
                 }
                 if (calcHours.DayOfWeek == DayOfWeek.Sunday)
                 {
                     Hours += 0.25;
-                    if ((skill == null) || (skill == "LEVEL3"))
+                    if ((v == null) || (v == "LEVEL3"))
                         LEVEL3SUN += 0.25;
-                    if (skill == "VANDVR")
+                    if (v == "VANDVR")
                         VANDVRSUN += 0.25;
-                    if (skill == "MR/HR")
+                    if (v == "MR/HR")
                         MRHRSUN += 0.25;
                 }
                 calcHours = calcHours.AddMinutes(15);
@@ -191,7 +181,34 @@ namespace PayEstimatorUWP
         }
     }
 
-    class Hours : INotifyPropertyChanged
+    public class Test : INotifyPropertyChanged
+    {
+        private double _test;
+
+        public double TotalTest
+        {
+            get { return _test; }
+            set
+            {
+                _test = value;
+                OnPropertyChanged("TotalHours");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        public Test()
+        {
+            TotalTest = 0.01;
+        }
+    }
+
+    public class Hours : INotifyPropertyChanged
     {
         private double _totalhours;
 
@@ -214,7 +231,7 @@ namespace PayEstimatorUWP
 
         public Hours(List<Gig> gigs)
         {
-            TotalHours = 0;
+            TotalHours = 0.00;
 
             foreach (Gig gig in gigs)
             {
@@ -223,7 +240,7 @@ namespace PayEstimatorUWP
         }
     }
 
-    class Gross : INotifyPropertyChanged
+    public class Gross : INotifyPropertyChanged
     {
         public double LEVEL3A { get; set; }
         public double LEVEL3B { get; set; }
@@ -234,6 +251,7 @@ namespace PayEstimatorUWP
         public double MRHRA { get; set; }
         public double MRHRB { get; set; }
         public double MRHRSUN { get; set; }
+
         private double _grossAmount;
 
         public double GrossAmount
@@ -283,4 +301,27 @@ namespace PayEstimatorUWP
             GrossAmount += MRHRA * 26 + MRHRB * 35 + MRHRSUN * 44;
         }
     }
+
+    public class DoubleToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return null;
+
+            string s = value.ToString();
+            return s;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
 }
+
