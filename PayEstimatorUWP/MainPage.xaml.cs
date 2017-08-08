5525bd8d30b068c203becb00a238ba1305f4785b
George Manning <george.manning@student.uts.edu.au>
2017-08-08 21:59:31 +1000

    Add PayRates source code file

--- PayEstimatorUWP/MainPage.xaml.cs
+++ PayEstimatorUWP/MainPage.xaml.cs
@@ -10,19 +10,6 @@ using Windows.UI.Xaml.Data;
 
 namespace PayEstimatorUWP
 {
-    static class PayRates
-    {
-        public static float LEVEL3A_RATE = 24.08f;
-        public static float LEVEL3B_RATE = 34.20f;
-        public static float LEVEL3SUN_RATE = 43.35f;
-        public static float VANDVRA_RATE = 25f;
-        public static float VANDVRB_RATE = 35f;
-        public static float VANDVRSUN_RATE = 44f;
-        public static float MRHRAA_RATE = 26f;
-        public static float MRHRAB_RATE = 35f;
-        public static float MRHRASUN_RATE = 44f;
-    }
-
     public sealed partial class MainPage : Page
     {
         public MainPage()
