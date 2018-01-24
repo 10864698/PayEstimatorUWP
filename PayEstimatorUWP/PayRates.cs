f2ffbd1e62717d30decea7dff5ff2508afb4d971
George Manning <george.manning@student.uts.edu.au>
2018-01-24 21:02:46 +1100

    Update rates

--- PayEstimatorUWP/PayRates.cs
+++ PayEstimatorUWP/PayRates.cs
@@ -4,15 +4,15 @@ namespace PayEstimatorUWP
 {
     static class PayRates
     {
-        public static float LEVEL3A_RATE = 24.08f;
-        public static float LEVEL3B_RATE = 34.20f;
+        public static float LEVEL3A_RATE = 26.11f;
+        public static float LEVEL3B_RATE = 37.08f;
         public static float LEVEL3SUN_RATE = 43.35f;
-        public static float VANDVRA_RATE = 26f;
-        public static float VANDVRB_RATE = 37.5f; //new rate 37.5
-        public static float VANDVRSUN_RATE = 44f;
-        public static float MRHRAA_RATE = 27f;  //new rate 27
-        public static float MRHRAB_RATE = 37.5f;
-        public static float MRHRASUN_RATE = 44f;
+        public static float VANDVRA_RATE = 26.50f;
+        public static float VANDVRB_RATE = 37.50f;
+        public static float VANDVRSUN_RATE = 48.00f;
+        public static float MRHRAA_RATE = 27.00f;
+        public static float MRHRAB_RATE = 37.50f;
+        public static float MRHRASUN_RATE = 48.00f;
     }
 }
 
